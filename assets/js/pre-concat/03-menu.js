cb02189403dcc4b1a09a2afaa9503d02be3ede46
chrisco97 <uchrisco97@aol.com>
2017-05-26 00:14:45 -0400

    Trying, yet again, to fix the background image issue on iOS devices

--- assets/js/pre-concat/03-menu.js
+++ assets/js/pre-concat/03-menu.js
@@ -3,21 +3,15 @@
 $(document).ready(function() {
   $('.menu-button').on('click', function() {
     if(!$('.menu-button').hasClass('open')) {
-      $(this).addClass('open');
-      $('header nav').addClass('open');
-      $('body').addClass('open');
+      $('.menu-button, header nav, body, html').addClass('open');
       $('.menu-overlay').fadeIn('fast');
      } else {
-      $(this).removeClass('open');
-      $('header nav').removeClass('open');
-      $('body').removeClass('open');
+      $('.menu-button, header nav, body, html').removeClass('open');
       $('.menu-overlay').fadeOut('fast');
     }
   }); 
   $('.menu-overlay').on('click', function() {
-    $('.menu-button').removeClass('open');
-    $('header nav').removeClass('open');
-    $('body').removeClass('open');
+    $('.menu-button, header nav, body, html').removeClass('open');
     $('.menu-overlay').fadeOut('fast');
   })
 });
