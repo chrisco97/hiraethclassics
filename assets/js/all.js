024092774bad41ac937e81150eda2d7150148fbe
chrisco97 <uchrisco97@aol.com>
2017-05-26 00:23:58 -0400

    Revert to initial solution

--- assets/js/all.js
+++ assets/js/all.js
@@ -10264,15 +10264,15 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('.menu-button').on('click', function() {
     if(!$('.menu-button').hasClass('open')) {
-      $('.menu-button, header nav, body, html').addClass('open');
+      $('.menu-button, header nav, body').addClass('open');
       $('.menu-overlay').fadeIn('fast');
      } else {
-      $('.menu-button, header nav, body, html').removeClass('open');
+      $('.menu-button, header nav, body').removeClass('open');
       $('.menu-overlay').fadeOut('fast');
     }
   }); 
   $('.menu-overlay').on('click', function() {
-    $('.menu-button, header nav, body, html').removeClass('open');
+    $('.menu-button, header nav, body').removeClass('open');
     $('.menu-overlay').fadeOut('fast');
   })
 });
